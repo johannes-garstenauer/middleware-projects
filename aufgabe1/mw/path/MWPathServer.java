41ec6e4266c653d4b26a819913dbf3cf2fef9ebc
johannes_g <johannes.garstenauer@fau.de>
2025-10-23 16:23:26 +0200

    Implement reduceFriendships method

--- aufgabe1/mw/path/MWPathServer.java
+++ aufgabe1/mw/path/MWPathServer.java
@@ -12,6 +12,8 @@ import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
 import java.util.*;
 
+//TODO: String int conversion are really silly here
+
 @Singleton
 @Path("path")
 public class MWPathServer {
@@ -47,36 +49,40 @@ public class MWPathServer {
 
     /***
      * Returns True if the friendshipId lists have a common element.
+     * Linear time complexity w.r.t. set sizes.
      */
-    private static boolean areConnected(List<String> listA, List<String> listB) {
-        Set<String> set = new HashSet<>(listA);
-
-        return listB.stream().anyMatch(set::contains);
+    private static boolean areConnected(Set<String> setA, Set<String> setB) {
+        return setB.stream().anyMatch(setA::contains);
     }
 
-    // TODO edge cases: -> sollten passen
-    //  1) start = end
-    //  2) path is just 1 person long
     private static Map<String, Collection<String>> reduceFriendships(int startID, int endID) {
+        Map<String, Collection<String>> result = new HashMap<>(Collections.emptyMap());
+
         Set<String> startFriendships = new HashSet<>(Collections.emptySet());
         startFriendships.add(String.valueOf(startID));
 
-        List<String> endFriendships = new ArrayList<>(Collections.emptyList());
+        Set<String> endFriendships = new HashSet<>(Collections.emptyList());
         endFriendships.add(String.valueOf(endID));
-/*
-        while (!areConnected(startFriendships, endFriendships)) {
+
+        do {
 
             //StartFreundeskreis := alle Nutzer , die von startID in i Schritten erreichbar sind;
             for (String startFriend: startFriendships) {
-                //startFriendships.addAll(getFriends(String.valueOf(startFriend));
+                List<String> tmp_friendships = getFriends(Integer.parseInt(startFriend));
+
+                result.put(startFriend, tmp_friendships);
+                startFriendships.addAll(tmp_friendships);
             }
 
             //EndFreundeskreis := alle Nutzer , die von endID in i Schritten erreichbar sind;
             for (String endFriend: endFriendships) {
-                //endFriendships.addAll(getFriends(String.valueOf(endFriend)));
+                List<String> tmp_friendships = getFriends(Integer.parseInt(endFriend));
+
+                result.put(endFriend, tmp_friendships);
+                endFriendships.addAll(tmp_friendships);
             }
-        }*/
-        //return startFriendships.addAll(endFriendships);
-        return null;
+        } while (!areConnected(startFriendships, endFriendships));
+
+        return result;
     }
 }
