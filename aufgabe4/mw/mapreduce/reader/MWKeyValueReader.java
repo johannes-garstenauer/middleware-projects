65eb870560e407ffaa4fb3923389346134732655
Moritz Palm <moritz.palm@web.de>
2025-12-04 13:54:59 +0100

    added KeyValueReader

--- aufgabe4/mw/mapreduce/reader/MWKeyValueReader.java
+++ aufgabe4/mw/mapreduce/reader/MWKeyValueReader.java
@@ -1,4 +1,31 @@
 package mw.mapreduce.reader;
 
+import mw.mapreduce.util.MWPair;
+
+import java.util.ArrayList;
+
 public class MWKeyValueReader {
+    public final String key;
+    MWKeyValueReader ( String key ){
+        this.key = key;
+    }
+
+    ArrayList<MWPair<String, String>> getPairs(String line) {
+        ArrayList<MWPair<String, String>> result = new  ArrayList<MWPair<String, String>>();
+        boolean endOfLine = false;
+        int start = 0;
+        while (start < line.length()) {
+            int end = line.indexOf(" ", start);
+            if (end == -1) {
+                end = line.length();
+            }
+            String value = line.substring(start, end);
+            if (!value.isEmpty()) {
+                result.add(new MWPair<>(key, value));
+            }
+            start = end + 1;
+
+        }
+        return result;
+    }
 }
