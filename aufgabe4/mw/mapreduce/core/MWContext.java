ee1dff9416a3566ff656cabafaab15d488cd8481
Moritz Palm <moritz.palm@web.de>
2025-12-05 16:03:41 +0100

    added Context Interface

--- aufgabe4/mw/mapreduce/core/MWContext.java
+++ aufgabe4/mw/mapreduce/core/MWContext.java
@@ -1,4 +1,14 @@
 package mw.mapreduce.core;
 
-public class MWContext {
+import java.io.IOException;
+
+import mw.mapreduce.util.MWPair;
+import mw.mapreduce.reader.MWReader;
+
+public interface MWContext<VALUETYPE> {
+    // Input
+    MWReader<MWPair<String, VALUETYPE>> getReader();
+    // Output
+    void write(String key, String value) throws IOException;
+    void outputComplete() throws IOException;
 }
