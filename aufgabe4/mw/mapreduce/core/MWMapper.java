ce92aef614f47dcdc7d21052ecdb6d016151f8e5
Markus Gerber <markus.gerber@fau.de>
2025-12-11 16:02:21 +0100

    add implementation for Mapper and Reducer

--- aufgabe4/mw/mapreduce/core/MWMapper.java
+++ aufgabe4/mw/mapreduce/core/MWMapper.java
@@ -1,7 +1,33 @@
 package mw.mapreduce.core;
 
+import java.util.Objects;
+
+import mw.mapreduce.reader.MWReader;
+import mw.mapreduce.util.MWPair;
+
 public class MWMapper implements Runnable {
-    public void run (){}
-    public void setContext ( MWMapContext context ){}
-    protected void map( String key , String value , MWContext <?> context ) throws Exception {}
+    private MWMapContext context = null;
+
+    public void run() {
+        // setContext should be called before with a valid context
+        Objects.requireNonNull(context);
+
+        MWReader<MWPair<String, String>> reader = context.getReader();
+        MWPair<String, String> pair;
+        try {
+            while ((pair = reader.read()) != null) {
+                map(pair.getKey(), pair.getValue(), context);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setContext(MWMapContext context) {
+        this.context = context;
+    }
+
+    protected void map(String key, String value, MWContext<?> context) throws Exception {
+        context.write(key, value);
+    }
 }
--- aufgabe4/mw/mapreduce/core/MWReducer.java
+++ aufgabe4/mw/mapreduce/core/MWReducer.java
@@ -1,8 +1,37 @@
 package mw.mapreduce.core;
 
+import java.util.Objects;
+
+import mw.mapreduce.reader.MWReader;
+import mw.mapreduce.util.MWPair;
+
 public class MWReducer implements Runnable {
-    public void run (){}
-    public void setContext ( MWReduceContext context ){}
-    protected void reduce ( String key , Iterable <String > values ,
-                            MWContext <?> context ) throws Exception {}
+    private MWReduceContext context = null;
+
+    public void run() {
+        // setContext should be called before with a valid context
+        Objects.requireNonNull(context);
+
+        MWReader<MWPair<String, Iterable<String>>> reader = context.getReader();
+        MWPair<String, Iterable<String>> pair;
+        try {
+            while ((pair = reader.read()) != null) {
+                reduce(pair.getKey(), pair.getValue(), context);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setContext(MWReduceContext context) {
+        this.context = context;
+    }
+
+    protected void reduce(String key, Iterable<String> values,
+            MWContext<?> context) throws Exception {
+
+        for (String value : values) {
+            context.write(key, value);
+        }
+    }
 }
