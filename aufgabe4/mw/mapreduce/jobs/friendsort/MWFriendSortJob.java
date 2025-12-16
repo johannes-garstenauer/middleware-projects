d2a0dfba7cc78dfb762fbafb2c6a8d6b227f4338
johannes_g <johannes.garstenauer@fau.de>
2025-12-16 14:40:33 +0100

    Refactor FriendSort - Fix Reader Bug

--- aufgabe4/mw/mapreduce/jobs/friendsort/MWFriendSortJob.java
+++ aufgabe4/mw/mapreduce/jobs/friendsort/MWFriendSortJob.java
@@ -7,6 +7,9 @@ import mw.mapreduce.reader.MWKeyValueReader;
 import mw.mapreduce.reader.MWLineReader;
 import mw.mapreduce.reader.MWReader;
 import mw.mapreduce.util.MWPair;
+import mw.mapreduce.util.MWSplitTextFileInput;
+
+import java.io.IOException;
 
 public class MWFriendSortJob extends MWJob {
 
@@ -22,5 +25,20 @@ public class MWFriendSortJob extends MWJob {
     @Override
     public java.util.Comparator<String> getComparator() {
         return new MWFriendSortComparator();
+    }
+
+    @Override
+    public MWReader<MWPair<String, String>> createInputReader(String filename, long start, long length) throws Exception {
+        MWSplitTextFileInput splitInput = new MWSplitTextFileInput(filename, start, length);
+
+        // Create an anonymous subclass of MWLineReader that delegates to splitInput
+        MWLineReader lineReader = new MWLineReader(filename) {
+            @Override
+            public String read() throws IOException {
+                return splitInput.readLine();
+            }
+        };
+
+        return new MWKeyValueReader(lineReader);
     }
 }
